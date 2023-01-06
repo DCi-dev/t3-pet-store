d24f7dfdf7fc29f70294fdf60beebed38446cfb8
DanCristianIlie <ilie.dancrist@protonmail.com>
2023-01-06 23:01:56 +0200

    style product list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,10 +24,14 @@ const Home: NextPage = ({
           </h2>
           <p>Cat food of many variations</p>
         </div>
-        <div className="mx-auto mt-8 grid grid-cols-1 gap-8 bg-neutral-700 p-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:p-36">
-          {products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
+        <div className="bg-white">
+          <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="grid max-w-7xl grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </div>
+          </div>
         </div>
         <FooterBanner footerBanner={bannerData.length && bannerData[1]} />
         Footer
