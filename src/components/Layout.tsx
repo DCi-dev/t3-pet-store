960e65ab89c1e94671f486aea2d851fcc508240a
DanCristianIlie <ilie.dancrist@protonmail.com>
2023-03-27 20:15:21 +0300

    fix accessibility

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,10 +8,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Head>
         <meta charSet="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
         <title>Pet Store</title>
         <meta name="description" content="Pet Food from the Future" />
         <link
