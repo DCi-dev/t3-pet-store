a0ae2249c4e2c561d1ce18b4da890d4eb515ac07
DanCristianIlie <ilie.dancrist@protonmail.com>
2023-03-27 19:46:35 +0300

    update pwa

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,21 +11,21 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/icons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/icons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/icons/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <script
           async
           defer
